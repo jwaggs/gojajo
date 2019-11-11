7e6976d4b5eb1c1f798830f09cd38cc0a095233d
Jon Waggoner <jwaggs27@gmail.com>
2019-11-11 14:00:09 -0500

    db connection

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,11 +3,30 @@ package main
 import (
 	"fmt"
 	"github.com/go-chi/chi"
+	"log"
 	"net/http"
 	"os"
 )
 
+import (
+	"database/sql"
+	_ "github.com/lib/pq"
+)
+
 func main() {
+	url := os.Getenv("DATABASE_URL")
+	db, err := sql.Open("postgres", url)
+	if err != nil {
+		log.Fatalln("db conn", url, "error", err)
+	}
+
+	log.Println("db open", db)
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatalln("db ping error", err)
+	}
+
 	r := chi.NewRouter()
 
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
@@ -16,9 +35,11 @@ func main() {
 
 	port := os.Getenv("PORT")
 	if port == "" {
+		// default if no env var exists
 		port = "3000"
 	}
 
 	addr := fmt.Sprintf(":%s", port)
+	log.Println("serving on", addr)
 	http.ListenAndServe(addr, r)
 }
