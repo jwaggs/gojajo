a6a84654118e1285758fb8dc68e56a297150a117
Jon Waggoner <jwaggs27@gmail.com>
2019-11-19 00:05:31 -0500

    proto generation

--- pb/gen.sh
+++ pb/gen.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/def" >/dev/null && pwd )"
-PIGGY_PROTO_PKG="/Users/jon/Documents/code/piggy/pb"
-PIGGY_SWAGGER_PKG="/Users/jon/Documents/code/piggy/pb"
+PIGGY_PROTO_DEF="$( cd "$( dirname "${BASH_SOURCE[0]}" )/def" >/dev/null && pwd )"
+PIGGY_GO_OUT="$( dirname "${BASH_SOURCE[0]}" )"
+PIGGY_SWAGGER_OUT="$( dirname "${BASH_SOURCE[0]}" )/swagger"
 
-ls $DIR/*.proto | while read -r line ; do
+ls $PIGGY_PROTO_DEF/*.proto | while read -r line ; do
     protoc -I$GOPATH/src \
-    --go_out=plugins=grpc:$PIGGY_PROTO_PKG \
-    --grpc-gateway_out=logtostderr=true:$PIGGY_PROTO_PKG \
-    --swagger_out=logtostderr=true:$PIGGY_SWAGGER_PKG \
-    --proto_path=$DIR $line
+    --go_out=plugins=grpc:$PIGGY_GO_OUT \
+    --grpc-gateway_out=logtostderr=true:$PIGGY_GO_OUT \
+    --swagger_out=logtostderr=true:$PIGGY_SWAGGER_OUT \
+    --proto_path=$PIGGY_PROTO_DEF $line
 done
