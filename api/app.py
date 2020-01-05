ca8bda96da976167643c1a8c607f88a04d164943
Jon Waggoner <jwaggs27@gmail.com>
2020-01-04 20:24:11 -0500

    initial migrations written

--- api/app.py
+++ api/app.py
@@ -2,14 +2,7 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-
-# from sqlalchemy import Table, Column, Integer, ForeignKey
 from sqlalchemy.orm import relationship
-from sqlalchemy.ext.declarative import declarative_base
-import sqlalchemy as sa
-
-Base = declarative_base()
-
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = f'postgres://fyparbfgmnlvnt:{os.getenv("DB_PASS")}@ec2-174-129-253-162.compute-1.amazonaws.com:5432/d93afugd4i967s'
@@ -18,114 +11,127 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 
-class Team(Base):
+class Team(db.Model):
+    """
+    The root model that users, banks, and bins relate to
+    """
     __tablename__ = 'team'
-    id = sa.Column(sa.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     users = relationship('User', back_populates='team')
     banks = relationship('Bank', back_populates='team')
     major_bins = relationship('MajorBin', back_populates='team')
     incomes = relationship('Income', back_populates='team')
 
-    name = sa.Column(sa.String(128))
+    name = db.Column(db.String(128))
 
 
-class User(Base):
+class User(db.Model):
+    """
+    An individual user
+    """
     __tablename__ = 'user'
-    id = sa.Column(sa.Integer, primary_key=True)
-    team_id = sa.Column(sa.Integer, sa.ForeignKey('team.id'))
+    id = db.Column(db.Integer, primary_key=True)
+    team_id = db.Column(db.Integer, db.ForeignKey('team.id'))
 
-    name = sa.Column(sa.String(128))
+    name = db.Column(db.String(128))
 
 
-class Bank(Base):
+class Bank(db.Model):
+    """
+    A Banking institution or logical grouping of transactional accounts
+    """
     __tablename__ = 'bank'
-    id = sa.Column(sa.Integer, primary_key=True)
-    team_id = sa.Column(sa.Integer, sa.ForeignKey('team.id'))
+    id = db.Column(db.Integer, primary_key=True)
+    team_id = db.Column(db.Integer, db.ForeignKey('team.id'))
     bank_accounts = relationship('BankAccount', back_populates='bank')
 
-    name = sa.Column(sa.String(128))
-    description = sa.Column(sa.String(128))
+    name = db.Column(db.String(128))
+    description = db.Column(db.String(128))
 
 
-class BankAccount(Base):
-    __tablename__ = 'bank_account'
-    id = sa.Column(sa.Integer, primary_key=True)
-    bank_id = sa.Column(sa.Integer, sa.ForeignKey('bank.id'))
-
-    name = sa.Column(sa.String(128))
-    description = sa.Column(sa.String(128))
-
-
-class Transaction(Base):
-    # TODO: add status such as pending/confirmed
-    __tablename__ = 'transaction'
-    id = sa.Column(sa.Integer, primary_key=True)
-    bank_account_id = sa.Column(sa.Integer, sa.ForeignKey('bank_account.id'))
-    minor_bin_id = sa.Column(sa.Integer, sa.ForeignKey('minor_bin.id'))
-
-    name = sa.Column(sa.String(128))
-    description = sa.Column(sa.String(128))
-    date_of_transaction = sa.Column(sa.DateTime)
-    date_created = sa.Column(sa.DateTime)
-    date_updated = sa.Column(sa.DateTime)
-
-
-class MajorBin(Base):
-    # TODO: color
-    __tablename__ = 'major_bin'
-    id = sa.Column(sa.Integer, primary_key=True)
-    team_id = sa.Column(sa.Integer, sa.ForeignKey('team.id'))
-
-    name = sa.Column(sa.String(128))
-    description = sa.Column(sa.String(128))
-
-
-class IncomeAllocation(Base):
+class BankAccount(db.Model):
     """
-    Joins the MinorBin and Income tables to track how much of an income is divided into each bin.
+    Encapsulates common data for a Bank's Accounts
+    """
+    __tablename__ = 'bank_account'
+    id = db.Column(db.Integer, primary_key=True)
+    bank_id = db.Column(db.Integer, db.ForeignKey('bank.id'))
+
+    name = db.Column(db.String(128))
+    description = db.Column(db.String(128))
+
+
+class Transaction(db.Model):
+    """
+    Encapsulates common data between BankAccount credits and debits
+    """
+    __tablename__ = 'transaction'
+    id = db.Column(db.Integer, primary_key=True)
+    bank_account_id = db.Column(db.Integer, db.ForeignKey('bank_account.id'))
+    minor_bin_id = db.Column(db.Integer, db.ForeignKey('minor_bin.id'))
+    # TODO: add status such as pending/confirmed
+    name = db.Column(db.String(128))
+    description = db.Column(db.String(128))
+    date_of_transaction = db.Column(db.DateTime)
+    date_created = db.Column(db.DateTime)
+    date_updated = db.Column(db.DateTime)
+
+
+class MajorBin(db.Model):
+    """
+    Allows for grouping of MinorBins together into a parent bin
+    """
+    __tablename__ = 'major_bin'
+    id = db.Column(db.Integer, primary_key=True)
+    team_id = db.Column(db.Integer, db.ForeignKey('team.id'))
+    # TODO: color
+    name = db.Column(db.String(128))
+    description = db.Column(db.String(128))
+
+
+class IncomeAllocation(db.Model):
+    """
+    Joins the MinorBin and Income tables to track how much of an income is divided into each bin
     """
     __tablename__ = 'income_allocation'
-    income_id = sa.Column(sa.Integer, sa.ForeignKey('minor_bin.id'), primary_key=True)
-    minor_bin_id = sa.Column(sa.Integer, sa.ForeignKey('income.id'), primary_key=True)
-# income_allocation = sa.Table('income_allocation', Base.metadata,
-#     sa.Column('income_id', sa.Integer, sa.ForeignKey('income.id')),
-#     sa.Column('minor_bin_id', sa.Integer, sa.ForeignKey('minor_bin.id')),
-#     sa.Column('amount', sa.Integer),
-#     sa.Column('interval_unit', sa.Integer),
-#     sa.Column('interval_type', sa.Column(sa.String(128)))  # TODO: change interval_type to enum
-# )
+    income_id = db.Column(db.Integer, db.ForeignKey('minor_bin.id'), primary_key=True)
+    minor_bin_id = db.Column(db.Integer, db.ForeignKey('income.id'), primary_key=True)
+
+    name = db.Column(db.String(128))
+    description = db.Column(db.String(128))
+    amount = db.Column(db.Float)
+    interval_unit = db.Column(db.Integer)
+    interval_type = db.Column(db.String(128))  # TODO: change interval_type to enum
 
 
-class MinorBin(Base):
+class MinorBin(db.Model):
     """
     Most granular form of bin tracking
     """
     __tablename__ = 'minor_bin'
-    id = sa.Column(sa.Integer, primary_key=True)
-    major_bin_id = sa.Column(sa.Integer, sa.ForeignKey('major_bin.id'))
+    id = db.Column(db.Integer, primary_key=True)
+    major_bin_id = db.Column(db.Integer, db.ForeignKey('major_bin.id'))
     incomes = relationship('Income', secondary='income_allocation', back_populates='minor_bins')
-    # incomes = relationship('Income', secondary=income_allocation, back_populates="minor_bins")
 
-    name = sa.Column(sa.String(128))
-    description = sa.Column(sa.String(128))
-    amount = sa.Column(sa.Float)
+    name = db.Column(db.String(128))
+    description = db.Column(db.String(128))
+    amount = db.Column(db.Float)
 
 
-class Income(Base):
+class Income(db.Model):
     """
     Tracks each type of income for a Team
     """
     __tablename__ = 'income'
-    id = sa.Column(sa.Integer, primary_key=True)
-    team_id = sa.Column(sa.Integer, sa.ForeignKey('team.id'))
+    id = db.Column(db.Integer, primary_key=True)
+    team_id = db.Column(db.Integer, db.ForeignKey('team.id'))
     minor_bins = relationship('MinorBin', secondary='income_allocation', back_populates='incomes')
-    # minor_bins = relationship('MinorBin', secondary=income_allocation_table, back_populates="incomes")
 
-    name = sa.Column(sa.String(128))
-    description = sa.Column(sa.String(128))
-    amount = sa.Column(sa.Float)
-    interval_unit = sa.Column(sa.Integer)
-    interval_type = sa.Column(sa.String(128))  # TODO: change interval_type to enum
+    name = db.Column(db.String(128))
+    description = db.Column(db.String(128))
+    amount = db.Column(db.Float)
+    interval_unit = db.Column(db.Integer)
+    interval_type = db.Column(db.String(128))  # TODO: change interval_type to enum
 
 
 @app.route('/')
